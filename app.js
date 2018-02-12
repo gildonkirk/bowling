d8ae08459851d7ef9004788aee4e13b7ee52b5de
Gildon Kirk <gildonkirk@gmail.com>
2018-02-12 17:56:50 -0500

    now turns array of strings to array of integers

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const scores = [];
+const scoresInt = [];
+emptyInput();
+
+$(document).on('click', '.scoreSubmit', function() {
+  inputToArray();
+  for(i = 0; i < scores.length; i++) {
+    let intValue = parseInt(scores[i]);
+    scoresInt.push(intValue);
+  }
+  // const value = parseInt(scores[0]);
+  console.log(scoresInt);
+});
+
+function inputToArray() {
+  const textInput = $('.scoreInput').val();
+  for(i = 0; i < textInput.length; i++) {
+    scores.push(textInput[i]);
+  };
+  console.log(scores);
+}
+
 function emptyInput() {
   $(document).on('click','.scoreInput', function() {
     if($('.scoreInput').val() === 'Enter Scores') {
@@ -6,12 +27,3 @@ function emptyInput() {
     }
   });
 }
-emptyInput();
-
-$(document).on('click', '.scoreSubmit', function() {
-  const textInput = $('.scoreInput').val();
-  for(i = 0; i < textInput.length; i++) {
-    scores.push(textInput[i]);
-  };
-  console.log(scores);
-});
