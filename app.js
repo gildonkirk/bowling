d04929e2fd51b94f9b9291964eec58ae07c21ca7
Gildon Kirk <gildonkirk@gmail.com>
2018-02-13 13:24:24 -0500

    parses all but x / -

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const scores = [];
+let scores = [];
 const scoresInt = [];
 emptyInput();
 
 $(document).on('click', '.scoreSubmit', function() {
-  inputToArray();
-  for(i = 0; i < scores.length; i++) {
-    let intValue = parseInt(scores[i]);
-    scoresInt.push(intValue);
-  }
-  // const value = parseInt(scores[0]);
-  console.log(scoresInt);
+  scores = [];
+  // inputToArray();
+  parseValues();
+
 });
 
-function inputToArray() {
+function parseValues() {
   const textInput = $('.scoreInput').val();
   for(i = 0; i < textInput.length; i++) {
-    scores.push(textInput[i]);
-  };
+    if(textInput[i] != '-' && textInput[i] != 'X' && textInput[i] != '/') {
+      let intValue = parseInt(textInput[i]);
+      scores.push(intValue);
+    } else {
+      scores.push(textInput[i]);
+    }
+  }
   console.log(scores);
-}
+};
+
+// function inputToArray() {
+//   const textInput = $('.scoreInput').val();
+//   for(i = 0; i < textInput.length; i++) {
+//     scores.push(textInput[i]);
+//   };
+//   console.log(scores);
+// }
 
 function emptyInput() {
   $(document).on('click','.scoreInput', function() {
